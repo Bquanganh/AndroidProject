bc3013247869ddd24a29a9b8dedc2ba09c236e74
EgbertDuncan <84497687+EgbertDuncan@users.noreply.github.com>
2022-05-24 23:24:32 +0700

    fix

--- app/src/main/java/com/example/test/SelectRegistrationActivity.java
+++ app/src/main/java/com/example/test/SelectRegistrationActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,33 +17,28 @@ public class SelectRegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_registration);
 
+
+
+
         donorButton = findViewById(R.id.donorButton);
-        recipientButton = findViewById(R.id.recipientButton);
+        donorButton.setOnClickListener(view -> {
+            Intent intent = new Intent (SelectRegistrationActivity.this, Donor.class);
+            startActivity(intent);
+        });
+
         backButton = findViewById(R.id.backButton);
-
-        donorButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent (SelectRegistrationActivity.this, Donor.class);
-                startActivity(intent);
-            }
+        backButton.setOnClickListener(view -> {
+            Intent intent = new Intent (SelectRegistrationActivity.this, LoginActivity.class);
+            startActivity(intent);
         });
 
-        recipientButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent (SelectRegistrationActivity.this, RecipientRegistrationActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        recipientButton = findViewById(R.id.recipientButton);
+        recipientButton.setOnClickListener(view -> {
+            Intent intent = new Intent (SelectRegistrationActivity.this, RecipientRegistrationActivity.class);
+            startActivity(intent);
+            finish();
         });
 
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent (SelectRegistrationActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+
     }
 }
